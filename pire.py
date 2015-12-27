35d90e6f37017cae4c4d47e7173de9ff85a93418
Caleb Ely <le717.code@yahoo.com>
2015-12-26 21:32:16 -0500

    Add module doctypes

--- pire.py
+++ pire.py
@@ -1,5 +1,14 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*
+"""pire - LEGO Racers mods package manager.
+
+Created 2015 Caleb Ely
+<http://codetriangle.me/>
+
+Licensed under The MIT License
+<http://opensource.org/licenses/MIT/>
+
+"""
 
 
 if __name__ == "__main__":
--- src/settings/app.py
+++ src/settings/app.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
+"""pire - LEGO Racers mods package manager.
+
+Created 2015 Caleb Ely
+<http://codetriangle.me/>
+
+Licensed under The MIT License
+<http://opensource.org/licenses/MIT/>
+
+"""
 
 
 import os
