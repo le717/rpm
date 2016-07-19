76ed3685f0b4c36e3561c7cdfa4af314325c31a3
Caleb Ely <le717.code@yahoo.com>
2016-07-19 18:42:38 -0400

    Expand settings help text

--- src/settings/help.py
+++ src/settings/help.py
@@ -8,6 +8,20 @@ def main():
     message = """Usage: {0} settings
 
 DESCRIPTION
-This command allows you to adjust user-defined settings.""".format(
+This command allows you to adjust user-defined settings.
+
+In order to install packages, you need to tell {0} where
+to find your LEGO Racers installation. Do this by running `{0} settings`
+and entering the path to your game. Do not point to any specific file,
+as that is not needed. Once a valid game installation is confirmed,
+{0} will configure itself to manage packages according to the
+game revision you own.
+
+If at any time your game installation changes location or needs to be
+configured, simply run the command again to restart the process.
+
+If you would like to learn more about the game differences between
+revisions, Rock Raiders United is an excellent resource for all things
+LEGO video games. http://www.rockraidersunited.com""".format(
         const.APP_NAME)
     print(message)
