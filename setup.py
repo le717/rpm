dc638c5dc4811b5ac06e3e5deaf424056f7170dd
Caleb Ely <le717.code@yahoo.com>
2017-11-18 16:55:17 -0500

    Add cx_Freeze script
    Currently faulty, having issues with cx_Freeze not picking up modules

---
+++ setup.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python3
+"""rpm - LEGO Racers mods package manager.
+
+Created 2015-2017 Caleb Ely
+<https://CodeTri.net/>
+
+Licensed under The MIT License
+<http://opensource.org/licenses/MIT/>
+
+"""
+
+
+import os
+from cx_Freeze import (setup, Executable)
+
+from src import constants as const
+
+
+# Output folder
+dest_folder = "bin"
+
+# Create the freeze path if it doesn't exist
+if not os.path.exists(dest_folder):
+    os.makedirs(dest_folder)
+
+build_exe_options = {
+    "build_exe": dest_folder,
+    "optimize": 2,
+    "bin_path_excludes": [".vscode", "tests"]
+}
+
+setup(
+    name=const.APP_NAME,
+    version=const.VERSION,
+    author=const.VERSION,
+    description="{0} v{1}".format(const.APP_NAME, const.VERSION),
+    license="MIT",
+    options={"build_exe": build_exe_options},
+    executables=[Executable("rpm.py", targetName="rpm.exe")]
+)
